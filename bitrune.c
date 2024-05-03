82dc2d4377543dabbd1d715c3c58cfeb3d0406e4
Paul Batchelor <thisispaulbatchelor@gmail.com>
2024-05-03 11:54:30 -0400

    bitrune: initial function prototypes

--- bitrune.c
+++ bitrune.c
@@ -1,1 +1,28 @@
 #include "bitrune.h"
+
+void bitrune_clear(bitrune_page *page)
+{
+
+}
+
+void bitrune_print(bitrune_page *page)
+{
+
+}
+
+void bitrune_set(bitrune_page *page, int x, int y, int s)
+{
+
+}
+
+int bitrune_extract(bitrune_page *page, bitrune_page *rune)
+{
+    return 1;
+}
+
+void bitrune_bounds(bitrune_page *rune,
+    int *x, int *y,
+    int *w, int *h)
+{
+
+}
--- bitrune.h
+++ bitrune.h
@@ -1,4 +1,27 @@
 #ifndef ORPH_BITRUNE_H
 #define ORPH_BITRUNE_H
 
+/* Page: 16x16 bitmap */
+
+typedef struct {
+    unsigned short rows[16];
+} bitrune_page;
+
+/* zero out and clear a page */
+void bitrune_clear(bitrune_page *page);
+
+/* print to stdout */
+void bitrune_print(bitrune_page *page);
+
+/* set a bitrune tile, given an X/Y location */
+void bitrune_set(bitrune_page *page, int x, int y, int s);
+
+/* extract the top-left most bitrune from page to rune */
+/* returns non-zero if no bitrune found */
+int bitrune_extract(bitrune_page *page, bitrune_page *rune);
+
+/* determine the bounding box of a rune or page */
+void bitrune_bounds(bitrune_page *rune,
+    int *x, int *y,
+    int *w, int *h);
 #endif
